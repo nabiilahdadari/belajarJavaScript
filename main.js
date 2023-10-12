e947cc6ae9bc6b8523f34a1a8ba111961b70d194
Nabiilah <117993237+nabiilahdadari@users.noreply.github.com>
2023-10-12 16:41:48 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// console.log("Hai")
+// alert("notifikasi")
+// prompt("pinjam seratus")
+
+// var promnet = "coding is easy"
+// console.log(promnet)
+// var promnet = "aku suka eskrim"
+// console.log(promnet)
+
+// let promnet = "coding is fun"
+// console.log(promnet)
+// let promnet = "coding is fun"
+// console.log(promnet)
+
+// const promnet = "coding"
+// console.log(promnet);
+// const promnet = "coding"
+// console.log(promnet);
+
+// let totalPoin = prompt("masukkan poin anda")
+//     if(totalPoin >= 100) {
+//         document.write("congrats")
+//     }
+//     else {
+//         document.write("keep trying")
+//     }
+        
+let x = 6; 
+let y = 3;
+
+console.log(x < 10 && y > 1)
+console.log(x < 10 && y < 1)
+console.log(x == 5 || y == 5)
+console.log(x == 6 || y == 5)
+console.log(!(x == y))
